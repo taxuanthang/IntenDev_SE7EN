14950237668ee0ffd55e9f9ff59d7ad9a4b5cf21
taxuanthang <128502415+taxuanthang@users.noreply.github.com>
2026-06-26 16:34:58 +0700

    [Feature] Nhân vật xoay theo hướng di chuyển

--- Assets/Script/PlayerLocomotionManager.cs
+++ Assets/Script/PlayerLocomotionManager.cs
@@ -15,10 +15,13 @@ public class PlayerLocomotionManager : MonoBehaviour
     public float verticalInput;
 
 
+
+
     public void Awake()
     {
         if (player == null) player = GetComponent<PlayerManager>();
     }
+
     public void HandleMoveInput(float horizontal_Input,float vertical_Input)
     {
         this.horizontalInput = horizontal_Input;
@@ -36,4 +39,19 @@ public class PlayerLocomotionManager : MonoBehaviour
 
         
     }
+
+    public void HandleRotation(float directionX, float directionY)
+    {
+        Vector3 targetDirection = Vector3.zero;
+        targetDirection = PlayerCamera.instance.transform.up * directionY;
+        targetDirection = targetDirection + PlayerCamera.instance.transform.right * directionX;
+        targetDirection.y = 0;
+        targetDirection.Normalize();
+        if(targetDirection == Vector3.zero)
+            targetDirection = transform.forward;
+        Quaternion tr = Quaternion.LookRotation(targetDirection);
+        Quaternion targetRotation = Quaternion.Slerp(transform.rotation, tr, player.playerLocomotionManager.moveAmount * Time.deltaTime * 20 );
+        transform.rotation = targetRotation;
+    }
+
 }
