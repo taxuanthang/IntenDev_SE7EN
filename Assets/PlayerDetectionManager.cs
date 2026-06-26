849b11b1feb1edbf43e8d52b8243df71a867786d
taxuanthang <128502415+taxuanthang@users.noreply.github.com>
2026-06-26 17:51:16 +0700

    [System] Thêm package TMP

---
+++ Assets/PlayerDetectionManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetectionManager : MonoBehaviour
+{
+    [Header("Ball Detection")]
+    public float detectBallRadius;
+    public LayerMask ballLayerMask;
+    public Vector3 height;
+    public bool debugDectectArea = false;
+
+
+    public void Update()
+    {
+        HandleBallDetection();
+    
+    }
+
+    public void HandleBallDetection()
+    {
+        // Detect Ball
+        Collider[] ballColliders = Physics.OverlapCapsule(this.transform.position, this.transform.position + height, detectBallRadius, ballLayerMask.value);
+
+        if (ballColliders.Length > 0)
+        {
+            foreach (Collider collider in ballColliders)
+            {
+                print(collider.name);
+            }
+
+
+        }
+
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (debugDectectArea)
+        {
+            Vector3 point1 = transform.position;
+            Vector3 point2 = transform.position + height;
+
+            Gizmos.color = Color.green;
+
+            // Hai đầu capsule
+            Gizmos.DrawWireSphere(point1, detectBallRadius);
+            Gizmos.DrawWireSphere(point2, detectBallRadius);
+
+            // Các cạnh capsule
+            Vector3 forward = transform.forward * detectBallRadius;
+            Vector3 right = transform.right * detectBallRadius;
+
+            Gizmos.DrawLine(point1 + right, point2 + right);
+            Gizmos.DrawLine(point1 - right, point2 - right);
+
+            Gizmos.DrawLine(point1 + forward, point2 + forward);
+            Gizmos.DrawLine(point1 - forward, point2 - forward);
+        }
+    }
+
+}
